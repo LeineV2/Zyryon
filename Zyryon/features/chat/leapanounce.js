688862e62d00f557749ba1d29152127488395ca3
LeineV2 <166173225+LeineV2@users.noreply.github.com>
2024-04-16 17:07:23 +0200

    Delete Zyryon/features/chat/leapanounce.js

--- Zyryon/features/chat/leapanounce.js
+++
@@ -1,7 +0,0 @@
-import config from "../../config";
-import variables from "../../utils/variables";
-
-register("chat", (name) => {
-    if(config.leapAnnounce)
-    ChatLib.command(`pc Leaped to ${name}!`)
-}).setCriteria("You have teleported to ${name}!")
