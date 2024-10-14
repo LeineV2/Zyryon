8caa53c53c81cb6f7384ecaec6ad0fc0934b695b
LeineV2 <166173225+LeineV2@users.noreply.github.com>
2024-10-14 18:21:33 +0200

    Delete features directory

--- features/Mining/MSBNotification.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="../../../CTAutocomplete" />
-import config from "../../config"
-
-register("chat", () => {
-    if(!config().MiningSpeedBoostNotifier) return
-    Client.showTitle(`&0&kKK&r &eMining Speed Boost &aReady!&r &0&kKK&r`, " ", 10, 25, 10)
-}).setCriteria("Mining Speed Boost is now available!")
--- features/chat/AntiEscrow.js
+++
@@ -1,6 +0,0 @@
-import config from "../../config";
-
-register("chat", () => {
-    if(!config().AntiEscrow) return
-    ChatLib.command("ah")
-}).setCriteria("&r&7Putting coins in escrow...&r")
